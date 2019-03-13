460884d85580bc10b6cecb58933ff31ceb3c3f2b
Mehmet AKTAS <aktasmehmet@gmail.com>
2019-03-13 20:52:51 +0300

    [8Puzzle] Dosya baslarina license bilgisi eklendi. maktas

--- sw/8Puzzle/board.cpp
+++ sw/8Puzzle/board.cpp
@@ -1,3 +1,12 @@
+/*
+ * 8Puzzle: Software for solving well known 8 puzzle problem
+ *
+ * BLG 521E Advanced Artificial Intelligence HW#1
+ *
+ * Copyright (C)  Mehmet AKTAS 0504181576 13.03.2019
+ *
+ */
+
 #include "board.h"
 #include "math.h"
 #include <QFile>
